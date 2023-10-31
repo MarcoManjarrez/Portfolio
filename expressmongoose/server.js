e9f23bf2b6ec4f3252efbbbcc48b46bb51c37e3f
marco <0253075@up.edu.mx>
2023-10-31 10:44:23 -0600

    changes

--- expressmongoose/server.js
+++ expressmongoose/server.js
@@ -2,9 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.engine("ejs", require("ejs").renderFile);
 app.set("view engine", "ejs");
@@ -12,7 +13,7 @@ app.set("view engine", "ejs");
 //const mongoUrl = "mongodb://127.0.0.1:27017/f1"; a local url
 const user = process.env.USER_ID;
 const pass = process.env.USER_PASS;
-const mongoUrl = `mongodb+srv://${user}:${pass}@cluster0.a84uqoe.mongodb.net/?retryWrites=true&w=majority`; //the url to mongo atlas
+const mongoUrl = `mongodb+srv://${user}:${pass}@cluster0.a84uqoe.mongodb.net/f1?retryWrites=true&w=majority`; //the url to mongo atlas
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Definition of a schema
@@ -61,22 +62,67 @@ let countries = [
 let teamsRaw = [  //used to add things to the database,as an example
   {code: "mercedes", name: "Mercedes", country: "GER"},
   {code: "aston_martin", name: "Aston Martin", country: "ENG"},
+  {code: "alpine", name: "Alpine", country: "FRA"},
+  {code: "hass_f1", name: "Hass F1 Team", country: "USA"},
+  {code: "red_bull", name: "Red Bull Racing", country: "AUS"},
+  {code: "alpha_tauri", name: "Alpha Tauri", country: "ITA"},
+  {code: "alpha_romeo", name: "Alpha Romeo", country: "ITA"},
+  {code: "ferrari", name: "Ferrari", country: "ITA"},
+  {code: "williams", name: "Williams", country: "ENG"},
+  {code: "mc_laren", name: "McLaren", country: "ENG"},
 ]
 
 let teams = [];
+let drivers = [];
+
 app.use("/", async(req, res, next)=>{
   if(teams.length == 0){
     var team = await Team.find({}).exec();
     if(!Array.isArray(team) || team.length == 0){
       //insert elements
+      await Team.insertMany(teamsRaw).then(()=>{ console.log("data loaded");}).catch((error)=>{console.error(error);});
+      await Team.find({}).then((docs)=>{console.log("Teams loaded"); teams = docs;}).catch((error)=>{console.error(error);});
     } else {
       teams = team;
     }
   }
 })
 
+app.post("/driver", async (req, res) => {
+  var team = await Team.findOne({code: {$eq:req.body.team}}).exec();
+  //Method 1 - Using the direct model
+  /*var driver = {
+    num: req.body.num,
+    code: req.body.code,
+    forename: req.body.name,
+    surname: req.body.lname,
+    dob: req.body.dob,
+    nationality: req.body.nation,
+    url: req.body.url,
+    team: team,
+  };
+  await Driver.insertOne(driver).then(()=>{ console.log("Driver recorded")}).catch((err)=>{
+    console.error(err);
+  });*/
+  //Method 2- Indirect use of model
+
+    var driver = new Driver({
+    num: req.body.num,
+    code: req.body.code,
+    forename: req.body.name,
+    surname: req.body.lname,
+    dob: req.body.dob,
+    nationality: req.body.nation,
+    url: req.body.url,
+    team: team,
+  });
+  driver.save();
+  drivers.push(driver);
+  res.redirect("/");
+ });
+
 app.get("/", (req, res) => {
- res.render("index", {countries, teams});
+ res.render("index", {countries, teams, drivers});
 });
 
 app.listen(3000, (err) => {
